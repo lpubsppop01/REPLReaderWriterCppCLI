92cbbb252b5d97203cf70502dfb8cedebbca85bd
lpubsppop01 <lpubsppop01@gmail.com>
2017-08-13 17:59:58 +0900

    Set class for internal use to private

--- REPLReaderWriter/Lock.h
+++ REPLReaderWriter/Lock.h
@@ -6,7 +6,7 @@ namespace REPLReaderWriter {
 // How to : Implement the lock C# Keyword(C++ - CLI):
 // https://msdn.microsoft.com/en-us/library/sy1y3y1t.aspx?f=255&MSPPError=-2147217396
 
-ref class Lock
+private ref class Lock
 {
     Object^ m_pObject;
 public:
